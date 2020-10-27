02024b4a1ee2019cf103a03cb2d329dc0ab29cb7
Fatima Rizvi <fatimarzv23@gmail.com>
2020-10-26 20:02:25 -0500

    MVP complete

--- src/snackBarApp/Main.java
+++ src/snackBarApp/Main.java
@@ -3,11 +3,11 @@ package snackBarApp;
 public class Main {
   public static void main(String[] args) {
 
+    // (String name, double cashOnHand)
     Customer jane = new Customer("Jane", 45.25);
     Customer bob = new Customer("Bob", 33.14);
 
-    System.out.println(jane.getName() + " cash on hand " + jane.getCashOnHand());
-
+    // (String name)
     VendingMachine food = new VendingMachine("Food");
     VendingMachine drink = new VendingMachine("Drink");
     VendingMachine office = new VendingMachine("Office");
@@ -20,7 +20,57 @@ public class Main {
     Snack soda = new Snack("Soda", 24, 2.50, drink.getId());
     Snack water = new Snack("Water", 20, 2.75, drink.getId());
 
-    //output
+    ///// processing and output
+
+    //Jane buys 3 sodas
+    jane.buySnacks(soda.totalCost(3));
+    soda.buySnacks(3);
+
+    System.out.println(jane.getName() + " cash on hand $" + jane.getCashOnHand());
+    System.out.println("Quantity left of " + soda.getName() + ": " + soda.getQuantity());
+    System.out.println();
+
+    //Jane buys 1 pretzel
+    jane.buySnacks(pretzel.totalCost(1));
+    pretzel.buySnacks(1);
+
+    System.out.println(jane.getName() + " cash on hand $" + jane.getCashOnHand());
+    System.out.println("Quantity left of " + pretzel.getName() + ": " + pretzel.getQuantity());
+    System.out.println();
+
+    //Bob buys 2 sodas
+    bob.buySnacks(soda.totalCost(2));
+    soda.buySnacks(2);
+
+    System.out.println(bob.getName() + " cash on hand $" + bob.getCashOnHand());
+    System.out.println("Quantity left of " + soda.getName() + ": " + soda.getQuantity());
+    System.out.println();
+
+    //Jane finds $10
+    jane.addCash(10);
+
+    System.out.println(jane.getName() + " cash on hand $" + jane.getCashOnHand());
+    System.out.println();
+
+    //Jane buys 1 chocolate bar
+    jane.buySnacks(chocolate.totalCost(1));
+    chocolate.buySnacks(1);
+
+    System.out.println(jane.getName() + " cash on hand $" + jane.getCashOnHand());
+    System.out.println("Quantity left of " + chocolate.getName() + ": " + chocolate.getQuantity());
+    System.out.println();
+
+    //Add 12 more pretzels
+    pretzel.addQuantity(12);
+    System.out.println("Quantity of " + pretzel.getName() + ": " + pretzel.getQuantity());
+
+    //Bob buys 3 pretzels
+    bob.buySnacks(pretzel.totalCost(3));
+    pretzel.buySnacks(3);
+
+    System.out.println(bob.getName() + " cash on hand $" + bob.getCashOnHand());
+    System.out.println("Quantity left of " + pretzel.getName() + ": " + pretzel.getQuantity());
+    System.out.println();
 
   }
 
